7b369bee84205d84e4e1028dc0b6b2c8170e4f52
devnoAnt <121880668+devnoAnt@users.noreply.github.com>
2024-05-02 04:37:56 +0000

    Add postgres

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     install_requires=[
         "dagster",
         "dagster-cloud",
+        "dagster_postgres",
         "pandas",
     ],
     extras_require={"dev": ["dagster-webserver", "pytest"]},
